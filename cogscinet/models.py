c627cfa4bf58a3811fb02f9f65577eec60856470
Tobias Thelen <tobias.thelen@uni-osnabrueck.de>
2022-05-31 21:03:15 +0200

    removed dummy database

--- cogscinet/models.py
+++ cogscinet/models.py
@@ -20,11 +20,12 @@ class Verein(models.Model):
     firstname = models.CharField(max_length=128)
     lastname = models.CharField(max_length=128)
     email = models.EmailField()
-    membership = models.CharField(max_length=1, choices=[(MEMBER_ALREADY, 'Already member' ), (MEMBER_NOW, 'New membership'), (MEMBER_LATER, 'Planned for later')])
-    iban = models.CharField(max_length=32, verbose_name='IBAN')  # will be checked via special form field
+    membership = models.CharField(max_length=1, choices=[(MEMBER_ALREADY, 'I am a member of F2IKW and want to stay a member.' ), (MEMBER_NOW, 'I want to become a member now and I\'ll be able to join the hybrid meeting on July, 16th, 16:00'), (MEMBER_LATER, 'I plan to become a member later (selected plan is not binding, but we will remind you)')])
     plan = models.CharField(max_length=1, choices=[(PLAN_REDUCED, 'Reduced (21€)'), (PLAN_NORMAL, 'Regular (42€)'), (PLAN_SENIOR, 'Senior (84€)'), (PLAN_SILVER, 'Company, silver member (420€)'), (PLAN_GOLD, 'Company, gold member (1.764€)')])
+    # iban = models.CharField(max_length=32, verbose_name='IBAN (only if you become a member now')  # will be checked via special form field
     extra_fee = models.IntegerField(verbose_name='Optional: Higher membership fee per year')
-    suggested_name = models.CharField(max_length=128, verbose_name='Suggested name for the association')
+    suggested_name = models.CharField(max_length=128, verbose_name='Suggested name for the association (examples suggested so far: "Coxi Club" or "CogSci Network"')
+    privacy = models.BooleanField(verbose_name="I agree to the data privacy declaration as given below")
     notes = models.TextField(blank=True)
     validation_id = models.UUIDField(editable=False)
     validated = models.BooleanField(default=False, editable=False)
