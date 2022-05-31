1a7b3e32a11502348b63c140e7838053899b3fcb
Tobias Thelen <tobias.thelen@uni-osnabrueck.de>
2022-05-31 21:21:42 +0200

    removed dummy database

--- cogscinet/models.py
+++ cogscinet/models.py
@@ -16,12 +16,15 @@ class Verein(models.Model):
     PLAN_SENIOR = 'S'
     PLAN_SILVER = 'I'
     PLAN_GOLD = 'G'
+    PLAN_INDIVIDUAL = 'L'
 
     firstname = models.CharField(max_length=128)
     lastname = models.CharField(max_length=128)
     email = models.EmailField()
-    membership = models.CharField(max_length=1, choices=[(MEMBER_ALREADY, 'I am a member of F2IKW and want to stay a member.' ), (MEMBER_NOW, 'I want to become a member now and I\'ll be able to join the hybrid meeting on July, 16th, 16:00'), (MEMBER_LATER, 'I plan to become a member later (selected plan is not binding, but we will remind you)')])
-    plan = models.CharField(max_length=1, choices=[(PLAN_REDUCED, 'Reduced (21€)'), (PLAN_NORMAL, 'Regular (42€)'), (PLAN_SENIOR, 'Senior (84€)'), (PLAN_SILVER, 'Company, silver member (420€)'), (PLAN_GOLD, 'Company, gold member (1.764€)')])
+    membership = models.CharField(max_length=1, choices=[(MEMBER_ALREADY, 'I am a member of F2IKW and want to stay a member.' ), (MEMBER_NOW, 'I want to become a member now and I\'ll be able to join the hybrid meeting on July, 13th, 18:00'), (MEMBER_LATER, 'I plan to become a member later (selected plan is not binding, but we will remind you)')])
+    plan = models.CharField(max_length=1, choices=[(PLAN_REDUCED, 'Reduced (21€)'), (PLAN_NORMAL, 'Regular (42€)'),
+                                                   (PLAN_SENIOR, 'Senior (84€)'), (PLAN_SILVER, 'Company, silver member (420€)'),
+                                                   (PLAN_GOLD, 'Company, gold member (42 * 42€ = 1.764€)'), (PLAN_INDIVDUAL, 'Individual amount > 84€ for individuals, > 1764€ for companys, given below)')])
     # iban = models.CharField(max_length=32, verbose_name='IBAN (only if you become a member now')  # will be checked via special form field
     extra_fee = models.IntegerField(verbose_name='Optional: Higher membership fee per year')
     suggested_name = models.CharField(max_length=128, verbose_name='Suggested name for the association (examples suggested so far: "Coxi Club" or "CogSci Network"')
--- uniforms/urls.py
+++ uniforms/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('/', TemplateView.as_view(template_name='empty.html')),
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html')),  # login form
 
     # forms
-    path('schnupper/', include('schnupper.urls')),  # registration schnupperstudium
+#    path('schnupper/', include('schnupper.urls')),  # registration schnupperstudium
     path('cogscinet/', include('cogscinet.urls')),  # registration f2ikw / cogscinet
 ]
